99d9b56bca6e670b305b60295e28182843cf1b6a
Romain <romain.rousseau5@gmail.com>
2020-06-02 17:31:49 +0200

    adding Layout to index page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import DashboardGrid from "../components/DashboardGrid";
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome, Matt!</h1>
+    <div className="container">
+      <Layout home>
+        <h1 className="title">Welcome, Matt!</h1>
+
+        <DashboardGrid></DashboardGrid>
+      </Layout>
     </div>
   );
 }
