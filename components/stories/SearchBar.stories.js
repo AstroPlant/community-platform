1ca6bf6cf491732a40a361e5a2616b4f581ae7f1
Romain <romain.rousseau5@gmail.com>
2020-06-02 16:50:55 +0200

    adding SearchBar component

---
+++ components/stories/SearchBar.stories.js
@@ -0,0 +1,14 @@
+import { withKnobs } from "@storybook/addon-knobs/react";
+import React from "react";
+import SearchBar from "../SearchBar";
+
+export default {
+  component: SearchBar,
+  title: "SearchBar",
+  decorators: [withKnobs],
+  excludeStories: /.*Data$/,
+};
+
+export const Default = () => {
+  return <SearchBar />;
+};
