a04b3a59a4e3e2f3f10830a39d8f9126106d911a
Romain <romain.rousseau5@gmail.com>
2020-06-04 15:08:29 +0200

    Adding ArticleLayout to the article page
    Commit SHA ArticleLayout was addded on :
    
    600377132c8211368ee5ed09f8d442c0d14ab90a

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,26 +1,15 @@
-import Layout from "../../components/Layout";
 import { getFullArticle } from "../../lib/community";
+import ArticleLayout from "../../components/ArticleLayout";
 
 export default function Article({ article }) {
   return (
     <div className="container">
-      <Layout>
-        <img
-          src={"http://localhost:1337" + article.cover.url}
-          alt={article.cover.caption}
-        />
-        <h1>{article.title}</h1>
-        <h6>
-          {article.author.username} on the {article.created_at}
-        </h6>
-        <p>{article.content}</p>
-      </Layout>
+      <ArticleLayout article={article} />
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
-  console.log(context);
   const article = await getFullArticle(context.params.slug);
 
   return {
