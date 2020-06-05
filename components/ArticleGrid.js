1a30adb1c244e536c80a6428f7d20c95a2e607df
Romain <romain.rousseau5@gmail.com>
2020-06-05 09:53:20 +0200

    fixing article grid rows

--- components/ArticleGrid.js
+++ components/ArticleGrid.js
@@ -10,7 +10,7 @@ const GridContainer = styled.div`
   display: grid;
   grid-gap: ${(props) => props.theme.gridGap};
   grid-template-columns: repeat(3, 1fr);
-  grid-template-rows: repeat(4, 1fr);
+  grid-template-rows: repeat(2, 1fr);
 
   padding: 2rem 0;
 `;
