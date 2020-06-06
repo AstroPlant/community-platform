ffd139ef6fd22b86c21b5058c3100825c4a9e0cd
Romain <romain.rousseau5@gmail.com>
2020-06-06 13:59:12 +0200

    adding placeholders in right column of map page

--- pages/map.js
+++ pages/map.js
@@ -1,5 +1,8 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
+import styled from "styled-components";
+import Card from "../components/cards/Card";
+import KitCountCard from "../components/cards/KitCountCard";
 import MapGrid from "../components/grids/MapGrid";
 import MainLayout from "../components/layouts/MainLayout";
 import { getKits } from "../services/data-api";
@@ -7,6 +10,13 @@ import { getKits } from "../services/data-api";
 const NoSSRMapBuilder = dynamic(() => import("../components/MapBuilder"), {
   ssr: false,
 });
+
+const MarginCard = styled(Card)`
+  && {
+    margin-bottom: 1rem;
+    height: unset;
+  }
+`;
 
 export default function Map({ kits }) {
   return (
@@ -22,7 +32,21 @@ export default function Map({ kits }) {
           <div>
             <NoSSRMapBuilder kits={kits}></NoSSRMapBuilder>
           </div>
-          <div />
+          <div>
+            <MarginCard>
+              <b>How to use the kit map ?</b>
+              <p>
+                Here you can find out what other AstroPlant kits around the
+                world are doing and what makes them unique! Try to hover on the
+                icons or typ a specific region, city or kitname to find out
+                more!
+              </p>
+            </MarginCard>
+
+            <KitCountCard title={"Active kits"} count={kits.length} />
+            <KitCountCard title={"Active countries"} count={12} />
+            <KitCountCard title={"Community Challenges Kits"} count={35} />
+          </div>
         </MapGrid>
       </MainLayout>
     </>
