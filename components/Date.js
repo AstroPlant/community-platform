df1f19f8812440e4c1b5629b52dbda82746795e5
Romain <romain.rousseau5@gmail.com>
2020-06-07 23:13:07 +0200

    adding classname to Date for customization

--- components/Date.js
+++ components/Date.js
@@ -2,11 +2,12 @@ import { format, parseISO } from "date-fns";
 import PropTypes from "prop-types";
 import React from "react";
 
-export default function Date({ dateString }) {
+export default function Date({ className, dateString }) {
   const date = parseISO(dateString);
-  return <p>{format(date, "MMMM d yyyy")}</p>;
+  return <p className={className}>{format(date, "MMMM d yyyy")}</p>;
 }
 
 Date.propTypes = {
+  className: PropTypes.string,
   dateString: PropTypes.string.isRequired,
 };
