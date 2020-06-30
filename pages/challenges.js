3ea1630f953cb683e4c8856fee9699657b458b90
Romain Rousseau <romain.rousseau5@gmail.com>
2020-06-30 13:40:41 +0200

    adding challenge page

---
+++ pages/challenges.js
@@ -0,0 +1,36 @@
+import styled from "styled-components";
+import InProgress from "../components/InProgress";
+import MainLayout from "../components/layouts/MainLayout";
+import { getChallenges } from "../services/community";
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  height: 100%;
+`;
+
+export default function Challenges({ challenges }) {
+  return (
+    <MainLayout pageTitle={"Community Challenges"}>
+      <Content>
+        <InProgress
+          title={"Currently in the works"}
+          details={
+            "We are currently working on defining and building community challenges in association with our partners. If you want to learn more about communtiy challenges or want to help building them, please shoot us a message on the Slack."
+          }
+        />
+      </Content>
+    </MainLayout>
+  );
+}
+
+export async function getServerSideProps() {
+  return {
+    props: {
+      challenges: await getChallenges(),
+    },
+  };
+}
