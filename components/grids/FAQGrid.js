e3cc8879a4fcc1d9a128286945a8d7584a64435b
Romain Rousseau <romain.rousseau5@gmail.com>
2020-08-26 16:25:38 +0200

    Adding placeholders for empty grids

--- components/grids/FAQGrid.js
+++ components/grids/FAQGrid.js
@@ -5,6 +5,10 @@ import FAQCard from "../../components/cards/FAQCard";
 import Breaks from "../../utils/breakpoints";
 import Grid from "./Grid";
 
+const EmptyGrid = styled.div`
+  padding: 2rem 0;
+`;
+
 const GridContainer = styled(Grid)`
   && {
     grid-template-columns: repeat(2, 1fr);
@@ -21,33 +25,41 @@ const Column = styled.div`
   align-items: flex-start;
 `;
 
-export default function FAQGrid(props) {
+export default function FAQGrid({ faqs }) {
   // Sorting questions to be placed into columns
   // Avoids big gaps on opposite side when a FAQ is open on one dide
   let leftQuestions = [];
   let rightQuestions = [];
 
-  for (let i = 0; i < props.faqs.length; i++) {
-    if (i < props.faqs.length / 2) {
-      leftQuestions.push(props.faqs[i]);
+  for (let i = 0; i < faqs.length; i++) {
+    if (i < faqs.length / 2) {
+      leftQuestions.push(faqs[i]);
     } else {
-      rightQuestions.push(props.faqs[i]);
+      rightQuestions.push(faqs[i]);
     }
   }
 
   return (
-    <GridContainer>
-      <Column>
-        {leftQuestions.map((faq) => (
-          <FAQCard key={faq.id} faq={faq} />
-        ))}
-      </Column>
-      <Column>
-        {rightQuestions.map((faq) => (
-          <FAQCard key={faq.id} faq={faq} />
-        ))}
-      </Column>
-    </GridContainer>
+    <>
+      {faqs.length === 0 ? (
+        <EmptyGrid>
+          <p>No FAQs were found.</p>
+        </EmptyGrid>
+      ) : (
+        <GridContainer>
+          <Column>
+            {leftQuestions.map((faq) => (
+              <FAQCard key={faq.id} faq={faq} />
+            ))}
+          </Column>
+          <Column>
+            {rightQuestions.map((faq) => (
+              <FAQCard key={faq.id} faq={faq} />
+            ))}
+          </Column>
+        </GridContainer>
+      )}
+    </>
   );
 }
 
