6b72331c55d5f9ab54b7cac4c9377681ab9edfa2
Romain Rousseau <romain.rousseau5@gmail.com>
2020-08-27 17:45:18 +0200

    Fixing positioning and styling of DropDown Menus

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,17 @@
 import Link from "next/link";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { useAuth } from "../providers/Auth";
 import MenuIcon from "../public/icons/menu.svg";
 import Notification from "../public/icons/notification.svg";
 import SearchIcon from "../public/icons/search.svg";
 import Breaks from "../utils/breakpoints";
-import { useOutsideClick } from "../utils/clickListener";
 import Avatar from "./Avatar";
 import Brand from "./Brand";
 import Button from "./Button";
 import Drawer from "./Drawer";
 import DropdownMenu from "./DropdownMenu";
 import HeaderLink from "./HeaderLink";
-import Icon from "./Icon";
 import SearchBar from "./inputs/SearchBar";
 
 const HeaderContainer = styled.header`
@@ -73,23 +71,15 @@ const UserHeaderTools = styled(ButtonsRow)`
   }
 `;
 
-const ClickableItem = styled.div`
-  cursor: pointer;
-`;
-
 const Separator = styled.div`
   display: block;
 
   height: 1px;
-  width: 100%;
+  width: 90%;
 
-  margin: 0.5rem 0;
+  margin: 0.5rem auto;
 
-  background-color: ${(props) => props.theme.light};
-`;
-
-const IconButtonHolder = styled(Icon)`
-  margin: 0;
+  background-color: rgba(256, 256, 256, 0.5);
 `;
 
 const HeaderButton = styled(Button)`
@@ -118,42 +108,8 @@ const DrawerMenuButton = styled(HeaderButton)`
 export default function Header() {
   const { user, isLogged } = useAuth();
 
-  // Variables to handles opening & closing dropdown menus
-  const [hideMenu, setHideMenu] = useState(true);
-  const [hideNotif, setHideNotif] = useState(true);
-
   const [openDrawer, setOpenDrawer] = useState(false);
   const [openSearch, setOpenSearch] = useState(false);
-
-  const ddMenuRef = useRef(null);
-  const ddMenuTriggerRef = useRef(null);
-
-  const ddNotifRef = useRef(null);
-  const ddNotifTriggerRef = useRef(null);
-
-  const ddMenuClick = useOutsideClick(
-    ddMenuRef,
-    ddMenuTriggerRef,
-    closeMenu.bind(this)
-  );
-
-  const ddNotificationsClick = useOutsideClick(
-    ddNotifRef,
-    ddNotifTriggerRef,
-    closeNotif.bind(this)
-  );
-
-  /**
-   * Changes the state of the selected menu
-   * @param {string} selected menus
-   */
-  function toggleDropdown(selected) {
-    if (selected === "Menu") {
-      setHideMenu(!hideMenu);
-    } else {
-      setHideNotif(!hideNotif);
-    }
-  }
 
   /**
    * Changes the state of the drawer
@@ -167,20 +123,6 @@ export default function Header() {
    */
   function toggleSearch() {
     setOpenSearch(!openSearch);
-  }
-
-  /**
-   * Closes the dropdown menu
-   */
-  function closeMenu() {
-    setHideMenu(true);
-  }
-
-  /**
-   * Closes the notification menu
-   */
-  function closeNotif() {
-    setHideNotif(true);
   }
 
   const menuLinks = [
@@ -263,18 +205,16 @@ export default function Header() {
 
             {isLogged ? (
               <UserHeaderTools>
-                <div
-                  ref={ddNotifTriggerRef}
-                  onClick={() => toggleDropdown("Notification")}
-                >
-                  <HeaderButton
-                    inverted
-                    color="dark"
-                    icon={<Notification />}
-                    onClick={() => toggleNotification()}
-                  />
-
-                  <DropdownMenu ref={ddNotifRef} hidden={hideNotif}>
+                <div>
+                  <DropdownMenu
+                    trigger={
+                      <HeaderButton
+                        inverted
+                        color="dark"
+                        icon={<Notification />}
+                      />
+                    }
+                  >
                     <b>Notifications</b>
                     <Separator />
                     <p>No notifications yet</p>
@@ -285,31 +225,28 @@ export default function Header() {
                   <HeaderButton color="primary" label="Share" />
                 </Link>
 
-                <ClickableItem
-                  ref={ddMenuTriggerRef}
-                  onClick={() => toggleDropdown("Menu")}
+                <DropdownMenu
+                  trigger={<HeaderAvatar size={40} avatar={user.avatar} />}
                 >
-                  <HeaderAvatar size={40} avatar={user.avatar} />
-                  <DropdownMenu ref={ddMenuRef} hidden={hideMenu}>
-                    <p>
-                      Signed in as <b>{user.username}</b>
-                    </p>
-                    <Link
-                      passHref
-                      href={"/users/[username]"}
-                      as={`/users/${user.username}`}
-                    >
-                      <a>My profile</a>
-                    </Link>
-                    <Link passHref href={"/settings"}>
-                      <a>Settings</a>
-                    </Link>
-                    <Separator />
-                    <Link passHref href={"/logout"}>
-                      <a>Log out</a>
-                    </Link>
-                  </DropdownMenu>
-                </ClickableItem>
+                  <b>{user.username}</b>
+
+                  <Separator />
+
+                  <Link
+                    passHref
+                    href={"/users/[username]"}
+                    as={`/users/${user.username}`}
+                  >
+                    <a>My profile</a>
+                  </Link>
+                  <Link passHref href={"/settings"}>
+                    <a>Settings</a>
+                  </Link>
+                  <Separator />
+                  <Link passHref href={"/logout"}>
+                    <a>Log out</a>
+                  </Link>
+                </DropdownMenu>
               </UserHeaderTools>
             ) : (
               <SignUpButtonHolder>
