d72d352c38e879448d7b9bd00c9d0c7322111837
Romain Rousseau <romain.rousseau5@gmail.com>
2020-09-03 15:31:04 +0200

    removing edition tools on article pages

--- pages/news/[slug].js
+++ pages/news/[slug].js
@@ -1,38 +1,17 @@
 import styled from "styled-components";
 import Article from "../../components/Article";
 import ArticleInfos from "../../components/ArticleInfos";
-import Button from "../../components/Button";
 import ArticleCard from "../../components/cards/ArticleCard";
 import Card from "../../components/cards/Card";
 import Grid from "../../components/grids/Grid";
 import PageLayout from "../../components/layouts/PageLayout";
 import { useAuth } from "../../providers/Auth";
 import { getFullArticle } from "../../services/community";
-import Breaks from "../../utils/breakpoints";
 
 const AuthorCard = styled(Card)`
   && {
     height: unset;
     margin: 2rem 0;
-  }
-`;
-
-const ToolsHolder = styled.div`
-  @media screen and (max-width: ${Breaks.large}) {
-    display: none;
-  }
-`;
-
-const ToolButtonRow = styled(Grid)`
-  && {
-    grid-template-columns: 1fr 1fr;
-    padding: 0.5rem 0;
-  }
-`;
-
-const ToolButton = styled(Button)`
-  && {
-    margin: 0;
   }
 `;
 
@@ -52,15 +31,6 @@ export default function ArticlePage({ article, related }) {
       <Grid>
         <Article article={article} />
         <div>
-          {isOwner && (
-            <ToolsHolder>
-              <h3>Tools</h3>
-              <ToolButtonRow>
-                <ToolButton disabled inverted label="Edit" color="secondary" />
-                <ToolButton disabled inverted label="Delete" color="error" />
-              </ToolButtonRow>
-            </ToolsHolder>
-          )}
           <h3>Author</h3>
           <AuthorCard>
             <ArticleInfos author={article.author} date={article.published_at} />
