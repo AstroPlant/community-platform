d9195ae8cb7978523ccea1e20799771d9e3ea620
Romain <romain.rousseau5@gmail.com>
2020-09-07 18:14:21 +0200

    migrating api database to postgres

--- api/config/database.js
+++ api/config/database.js
@@ -1,15 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "postgres",
+        host: env("DATABASE_HOST", "localhost"),
+        port: env.int("DATABASE_PORT", 5432),
+        database: env("DATABASE_NAME", "strapi"),
+        username: env("DATABASE_USERNAME", "strapi"),
+        password: env("DATABASE_PASSWORD", "strapi"),
+        schema: "public"
       },
       options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
+        useNullAsDefault: true
+      }
+    }
+  }
 });
